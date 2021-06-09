8f51c0137fa77f3f7df2f8c880822ab30e6c201e
Kevin Counihan <kevin.counihan17@gmail.com>
2021-06-09 15:26:12 -0700

    updated images and content

--- start_server.js
+++ start_server.js
@@ -4,7 +4,7 @@ const { exec } = require("child_process");
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "")));
 
 app.listen(port, () => {
   console.log(
